6341920e23baa28ffa7378a811b86543390bcb32
Yakiba1 <Maycop.gq@gmail.com>
2024-08-20 10:50:26 +0500

    подключил Firebase, создал пост с fetch, создал AppPeopleList для вывода данных, реализовал запрос с помощю axios

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,51 @@
 
       <button class="btn primary" :disabled="name.length === 0">Создать</button>
     </form>
+
+    <AppPeopleList
+        @load="loadPeople"
+        :people="people">
+    </AppPeopleList>
   </div>
 </template>
 
 <script>
+import AppPeopleList from "@/components/AppPeopleList.vue";
+import axios from "axios";
+
 export default {
+  components: {AppPeopleList},
   data() {
     return {
-      name:''
+      name:'',
+      people: []
     }
   },
   methods: {
-    createPerson() {}
+    async createPerson() {
+      const response = await fetch('https://vue-with-http-50965-default-rtdb.firebaseio.com/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      })
+      const fireBaseData = await response.json()
 
+      console.log(fireBaseData)
+      this.name = ''
+    },
+    async loadPeople() {
+      const {data} = await axios.get('https://vue-with-http-50965-default-rtdb.firebaseio.com/people.json')
+      this.people = Object.keys(data).map(key => {
+        return {
+          id: key,
+          firstName: data[key].firstName, // или ...data[key]
+        }
+      }) // Вернет криптографические ключи. Map на каждой итерации получаем ключ где он будет равняться (oisagorjwerihw)
+    }
   }
 }
 </script>
